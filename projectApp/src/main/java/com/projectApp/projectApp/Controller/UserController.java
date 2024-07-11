850ad10772d21f404e4ddaf1b5a3c7049bb381b9
Pruthuvi Samod <pruthuvisamod@icloud.com>
2024-07-11 17:13:19 +0530

    changes to backend

--- projectApp/src/main/java/com/projectApp/projectApp/Controller/UserController.java
+++ projectApp/src/main/java/com/projectApp/projectApp/Controller/UserController.java
@@ -46,13 +46,13 @@ public class UserController {
             switch (message) {
                 case "user found":
                     return ResponseEntity.status(HttpStatus.OK)
-                            .body(new Response("success", "User found with provided credentials"));
+                            .body(new Response("success", "Login succesfull"));
                 case "user not found in given email":
                     return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                            .body(new Response("error", "No user found with provided email"));
+                            .body(new Response("error", "User not found for given email"));
                 default:
                     return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                            .body(new Response("error", "Password is incorrect for given email"));
+                            .body(new Response("error", "Invalid password"));
 
             }
         } catch (DatabaseException e) {
